dea2af0f83acc180c2ae641b6b3f273380113f7d
Haseeb Zahid <haseezahid0008@gmail.com>
2025-09-27 07:24:29 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpKLhf0scc4y68QqEAUe59tkQuRvehGMA",
+  authDomain: "js-glow-757f3.firebaseapp.com",
+  projectId: "js-glow-757f3",
+  storageBucket: "js-glow-757f3.appspot.com",
+  messagingSenderId: "663931171871",
+  appId: "1:663931171871:web:7211172fbea13f8e95912a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
